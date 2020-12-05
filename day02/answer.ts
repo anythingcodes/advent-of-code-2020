3e2a442b60dabcf6d714f5fcf15940c72dd2d5b2
Liz Shaw <liz.h.shaw@gmail.com>
2020-12-05 16:28:04 -0500

    Switch to fancy regex for day 2 solution  💃

--- day02/answer.ts
+++ day02/answer.ts
@@ -1,31 +1,40 @@
 interface IEntry {
-  first: number;
-  second: number;
-  char: string;
-  pass: string;
+  varA: number;
+  varB: number;
+  letter: string;
+  password: string;
 }
+
 export const processInput = (input: string): IEntry[] => {
+  const passwordRegex = /(?<varA>\d+)-(?<varB>\d+) (?<letter>[a-z]):\s(?<password>\w{1,})/i;
   return input.split('\n').map((x) => {
-    const [policy, pass] = x.split(':');
-    const [counts, char] = policy.split(' ');
-    const [first, second] = counts.split('-').map((y) => parseInt(y, 10));
-    return {first, second, char, pass};
+    const match = x.match(passwordRegex);
+    if (match && match.groups) {
+      const {groups: {letter, password, varA, varB}} = match;
+      return {
+        letter,
+        password,
+        varA: parseInt(varA, 10),
+        varB: parseInt(varB, 10),
+      };
+    }
+    return {} as IEntry;
   });
 };
 
 export const partOne = (input: IEntry[]) => {
   return input.reduce((curr, x) => {
-    const charCount = x.pass.replace(new RegExp(`[^${x.char}]`, 'g'), '').length;
-    return charCount >= x.first && charCount <= x.second ? curr + 1 : curr;
+    const charCount = x.password.replace(new RegExp(`[^${x.letter}]`, 'g'), '').length;
+    return charCount >= x.varA && charCount <= x.varB ? curr + 1 : curr;
   }, 0);
 };
 
 export const partTwo = (input: IEntry[]) => {
   return input.reduce((curr, x) => {
-    const passArray = x.pass.split('');
-    if (passArray[x.first] === x.char && passArray[x.second] !== x.char) {
+    const passArray = x.password.split('');
+    if (passArray[x.varA - 1] === x.letter && passArray[x.varB - 1] !== x.letter) {
       return curr + 1;
-    } else if (passArray[x.first] !== x.char && passArray[x.second] === x.char) {
+    } else if (passArray[x.varA - 1] !== x.letter && passArray[x.varB - 1] === x.letter) {
       return curr + 1;
     }
     return curr;
