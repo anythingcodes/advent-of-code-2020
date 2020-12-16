31b89c761cc9999e458c52a271749b5cdd1af73c
Liz Shaw <liz.h.shaw@gmail.com>
2020-12-15 20:54:30 -0500

    Add day 15 part 2

--- day15/answer.spec.ts
+++ day15/answer.spec.ts
@@ -1,29 +1,51 @@
-import {partOne} from "./answer";
+import {getTurnCount} from "./answer";
 
 describe('Part One', () => {
   it('returns correct answer for sample input.', () => {
-    expect(partOne([0, 3, 6])).toBe(436);
-    expect(partOne([1, 3, 2])).toBe(1);
-    expect(partOne([2, 1, 3])).toBe(10);
-    expect(partOne([1, 2, 3])).toBe(27);
-    expect(partOne([2, 3, 1])).toBe(78);
-    expect(partOne([3, 2, 1])).toBe(438);
-    expect(partOne([3, 1, 2])).toBe(1836);
+    expect(getTurnCount([0, 3, 6], 2020)).toBe(436);
+    expect(getTurnCount([1, 3, 2])).toBe(1);
+    expect(getTurnCount([2, 1, 3])).toBe(10);
+    expect(getTurnCount([1, 2, 3])).toBe(27);
+    expect(getTurnCount([2, 3, 1])).toBe(78);
+    expect(getTurnCount([3, 2, 1])).toBe(438);
+    expect(getTurnCount([3, 1, 2])).toBe(1836);
   });
 
   it('returns correct answer for question input', () => {
-    expect(partOne([15, 5, 1, 4, 7, 0])).toBe(1259);
+    expect(getTurnCount([15, 5, 1, 4, 7, 0])).toBe(1259);
   });
 });
 
 describe('Part Two', () => {
-  // it('returns correct answer for sample input.', () => {
-  //   expect(partTwo(SAMPLE_INPUT_2)).toBe(208);
-  // });
+  it('returns correct answer for sample input.', () => {
+    expect(getTurnCount([0, 3, 6], 30000000)).toBe(175594);
+  });
 
-  // it('returns correct result for question input', () => {
-  //   const result = partTwo(QUESTION_INPUT);
-  //   expect(result).toBe(2667858637669);
-  // });
+  it('returns correct answer for sample input.', () => {
+    expect(getTurnCount([1, 3, 2], 30000000)).toBe(2578);
+  });
 
+  it('returns correct answer for sample input.', () => {
+    expect(getTurnCount([2, 1, 3], 30000000)).toBe(3544142);
+  });
+
+  it('returns correct answer for sample input.', () => {
+    expect(getTurnCount([1, 2, 3], 30000000)).toBe(261214);
+  });
+
+  it('returns correct answer for sample input.', () => {
+    expect(getTurnCount([2, 3, 1], 30000000)).toBe(6895259);
+  });
+
+  it('returns correct answer for sample input.', () => {
+    expect(getTurnCount([3, 2, 1], 30000000)).toBe(18);
+  });
+
+  it('returns correct answer for sample input.', () => {
+    expect(getTurnCount([3, 1, 2], 30000000)).toBe(362);
+  });
+
+  it('returns correct answer for question input.', () => {
+    expect(getTurnCount([15, 5, 1, 4, 7, 0], 30000000)).toBe(689);
+  });
 });
--- day15/answer.ts
+++ day15/answer.ts
@@ -1,44 +1,13 @@
-export const partOne = (startingNumbers: number[]): number => {
-  const turns: number[] = [...startingNumbers];
 
-  for (let i = turns.length; i < 2020; i++) {
-    const prevNum = turns[i - 1];
-    const lastIndex = turns.lastIndexOf(prevNum, turns.length - 2);
+export const getTurnCount = (startingNumbers: number[], length: number = 2020) => {
+  let prevNum = startingNumbers[startingNumbers.length - 1];
+  const turns = new Array(length);
+  startingNumbers.forEach((num, i) => { turns[num] = i + 1; });
 
-    if (lastIndex > -1) {
-      turns.push(i - (lastIndex + 1));
-    } else {
-      turns.push(0);
-    }
+  for (let i = startingNumbers.length; i < length; i++) {
+    const next = turns[prevNum] ? i - turns[prevNum] : 0;
+    turns[prevNum] = i;
+    prevNum = next;
   }
-  return turns[turns.length - 1];
+  return prevNum;
 };
-
-// export const partTwo = (lines: string[]): number => {
-//   let mask: string;
-//   const memory: {[index: string]: number} = {};
-
-//   for (const line of lines) {
-//     const parsedMask = line.match(/^mask = (?<mask>[01X]+)$/);
-//     if (parsedMask && parsedMask.groups) {
-//       mask = parsedMask.groups.mask.split("").reverse().join("");
-//       continue;
-//     }
-
-//     const parsedAssignment = line.match(/^mem\[(?<index>\d+)\] = (?<value>\d+)$/);
-//     if (!parsedAssignment || !parsedAssignment.groups) {
-//       throw new Error(`Invalid line "${line}"`);
-//     }
-
-//     const addresses = calculateAddresses(mask!, memory, Number(parsedAssignment.groups.index).toString(2).split("")
-// .reverse());
-//     const value = Number(parsedAssignment.groups.value);
-//     for (const address of addresses) {
-//       memory[address] = value;
-//     }
-//   }
-
-//   const mapped = Object.keys(memory).map((address) => memory[address]);
-//   return mapped.reduce((sum, prev) => sum + prev, 0);
-
-// };
